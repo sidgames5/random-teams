9fa171a9b39838e7352a0b1d127adde0121b73b8
Sid <saytleader@outlook.com>
2023-10-05 09:10:30 -0400

    Update Main.hx

--- src/Main.hx
+++ src/Main.hx
@@ -6,16 +6,14 @@ class Main {
 
 	static var teams = new Array<Array<Int>>();
 	public static function main() {
-		var people = 0;
-		var teamSize = 0;
-		var runs = 0;
+		
 		Sys.println("Please assign each person to a number (first student is numbered 0)");
 		Sys.print("Number of students: ");
-		people = Std.parseInt(Sys.stdin().readLine());
+		var people = Std.parseInt(Sys.stdin().readLine());
 		Sys.print("Number of students per team: ");
-		teamSize = Std.parseInt(Sys.stdin().readLine());
+		var teamSize = Std.parseInt(Sys.stdin().readLine());
 		Sys.print("Number of runs: ");
-		runs = Std.parseInt(Sys.stdin().readLine());
+		var runs = Std.parseInt(Sys.stdin().readLine());
 
 		/*Thread.create(() -> {
       			for (m in motd) {
